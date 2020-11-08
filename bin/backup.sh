fab557755ba730ddfa2aa9676b8106644cb4a30e
Felipe Kenzo <Kenzo12152@gmail.com>
2020-11-08 15:55:04 -0300

    rename .dotfile

--- bin/backup.sh
+++ bin/backup.sh
@@ -1,22 +1,22 @@
 #!bin/bash
 
 # backup bin script files
-cp ~/bin ~/dotfile/ -r
+cp ~/bin ~/.dotfile/ -r
 
 # backup vim setting
-cp ~/.vimrc ~/dotfile/.vimrc
-cp ~/.vim/rc/dein.toml ~/dotfile/.vim/rc/dein.toml
-cp ~/.vim/rc/dein_lazy.toml ~/dotfile/.vim/rc/dein_lazy.toml
+cp ~/.vimrc ~/.dotfile/.vimrc
+cp ~/.vim/rc/dein.toml ~/.dotfile/.vim/rc/dein.toml
+cp ~/.vim/rc/dein_lazy.toml ~/.dotfile/.vim/rc/dein_lazy.toml
 
 # backup msys2 bash setting
-cp ~/.bashrc ~/dotfile/.bashrc
+cp ~/.bashrc ~/.dotfile/.bashrc
 
-cp ~/.bash_profile ~/dotfile/.bash_profile
-cp ~/.bash_aliases ~/dotfile/.bash_aliases
-cp ~/.bash_function ~/dotfile/.bash_function
+cp ~/.bash_profile ~/.dotfile/.bash_profile
+cp ~/.bash_aliases ~/.dotfile/.bash_aliases
+cp ~/.bash_function ~/.dotfile/.bash_function
 
 # backup .gitconfig
-cp ~/.gitconfig ~/dotfile/.gitconfig
+cp ~/.gitconfig ~/.dotfile/.gitconfig
 
 # backup .wslconfig
-cp ~/.wslconfig ~/dotfile/.wslconfig
+cp ~/.wslconfig ~/.dotfile/.wslconfig
